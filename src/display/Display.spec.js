6e6537e043c6a8276e5b1484536ae659b07e2b38
Bayron Puac <bayronpuac@gmail.com>
2019-10-14 19:10:36 -0500

    Reached MPV but not sure if I did it right.

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -7,3 +7,37 @@ test('Dispaly renders correctly', () => {
     render(<Display />)
 })
 
+test('shows open and unlocked on render', () => {
+    const { getByText } = render(<Display />)
+    getByText('Open')
+    getByText('Unlocked')
+})
+
+test('can show closed and locked', () => {
+    const { getByText} = render(
+    <Display 
+    locked={true} 
+    closed={true}
+    />
+    )
+    getByText('Locked')
+    getByText('Closed')
+})
+
+test('green led', () => {
+    const { getByText } = render(
+    <Display 
+    closed={false} 
+    locked={false}
+    />
+    )
+    expect(getByText('Open'))
+})
+
+test('red led', () => {
+    const { getByText } = render(
+    <Display closed={true} locked={true}
+    />
+    )
+    expect(getByText('Closed'))
+}) 
